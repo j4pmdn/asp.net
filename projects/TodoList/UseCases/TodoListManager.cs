aff24fe9cf15ea671b437cc9b3a26c95450a7ad9
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-03-22 23:27:40 +0700

    5. Todolist program

---
+++ projects/TodoList/UseCases/TodoListManager.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+
+namespace UseCases
+{
+    public class TodoListManager(ITodoItemRepository repository)
+    {
+        private readonly ITodoItemRepository repository = repository;
+
+        public IEnumerable<TodoItem> GetTodoItems()
+        {
+            return repository.GetItems();
+        }
+
+        public void AddTodoItem(TodoItem item)
+        {
+            repository.Add(item);
+        }
+
+        public void MarkComplete(int id)
+        {
+            var item = repository.GetById(id);
+            if (item != null)
+            {
+                item.IsCompleted = true;
+                repository.Update(item);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+    }
+}
